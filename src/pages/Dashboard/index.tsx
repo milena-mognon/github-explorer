f0e2b8671efb97040ce4cd7502345d68b2d0ec4f
milena-mognon <milenamognon@gmail.com>
2020-05-02 13:35:21 -0300

    configurations and routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// React.FC -> componente escriro em formato de função
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// React.FC -> componente escriro em formato de função
+const Repository: React.FC = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
